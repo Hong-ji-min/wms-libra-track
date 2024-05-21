a398f3d0163631ff7a2c8d3986b36fdbd67705d7
Hong-ji-min <jmh907@naver.com>
2024-05-21 16:56:31 +0900

    Feat : Member.sql 추가

---
+++ src/main/resources/sql/member/Member.sql
@@ -0,0 +1,15 @@
+insert into
+    member(name, username, password, role, phone_number, email)
+values
+    ('우희민', '희민', '희민', 'Admin', '12345678', 'WOOOO@whm.com'),
+    ('홍지민', '지민', '지민', 'ADMIN', '23456789', 'HOOONG@hjm.com'),
+    ('박민혁', '민혁', '민혁', 'ADMIN', '34567890', 'PAARK@pmh.com'),
+    ('심재람', '재람', '재람', 'ADMIN', '45678901', 'SSSIM@sjr.com'),
+    ('오형동', '형동', '형동', 'ADMIN', '10101010', 'OOOHOH@oh.com'),
+    ('박태준', '태준', '태준', 'ADMIN', '67890123', 'PARPARK@ptj.com'),
+    ('류선재', '선재', '선재', 'PUBLISHER', '99546325', 'SUN@sun.com'),
+    ('이유민', '유민', '유민', 'PUBLISHER', '99546325', 'REASON@reason.com'),
+    ('김범주', '범주', '범주', 'PUBLISHER', '39578582', 'CATEGORY@category.com'),
+    ('정새배', '새배', '새배', 'INVENTORY', '198546325', 'NEW@new.com'),
+    ('박대기', '대기', '대기', 'INVENTORY', '77777777', 'WAIT@wiat.com'),
+    ('최선중', '선중','선중', 'INVENTORY', '12345678', 'BEST@best.com');
